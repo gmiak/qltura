bce1306309d9b2d61dc31487e7ea31ac5c808761
gmiak <kayss.g@gmail.com>
2022-07-01 20:07:52 +0200

    Starting with server layer - Profile -

---
+++ lib/server/model/profile_interface.dart
@@ -0,0 +1,10 @@
+class ProfileInterface {
+  int? id;
+  String? firstname;
+  String? lastname;
+  String? username;
+  String? email;
+  String? gender;
+  List<String>? followers;
+  List<String>? following;
+}
