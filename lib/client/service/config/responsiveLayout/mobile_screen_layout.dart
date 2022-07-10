2cad5c46731cd5b6ffb901bd332f2038b48915b9
gmiak <kayss.g@gmail.com>
2022-07-10 23:52:10 +0200

    Added bottombar

--- lib/client/service/config/responsiveLayout/mobile_screen_layout.dart
+++ lib/client/service/config/responsiveLayout/mobile_screen_layout.dart
@@ -1,6 +1,15 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qltura/client/service/config/provider/user_provider.dart';
+import 'package:qltura/client/service/utils/colors.dart';
+import 'package:qltura/client/view/screens/discover_screen.dart';
+import 'package:qltura/client/view/screens/home_screen.dart';
+import 'package:qltura/client/view/screens/menu_screen.dart';
+import 'package:qltura/client/view/screens/notifications_screen.dart';
+import 'package:qltura/client/view/screens/profile_screen.dart';
 import 'package:qltura/server/controller/user_controller.dart';
 import 'package:qltura/server/model/user_model.dart';
 
@@ -14,22 +23,96 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
   void signout() {
     final us = UserController();
     us.logout();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     UserQ? user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(
-          child: Column(
+      body: PageView(
+        physics:
+            NeverScrollableScrollPhysics(), // can be usefull when implemented the scrolling categories on the user's profile
+        controller: pageController,
+        onPageChanged: onPageChanged,
         children: [
-          Text(user.username),
-          IconButton(onPressed: signout, icon: const Icon(Icons.logout))
+          HomeScreen(),
+          DiscoverScreen(),
+          ProfileScreen(),
+          NotificationScreen(),
+          MenuScreen()
         ],
-      )),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        activeColor: secondaryColor,
+        inactiveColor: secondaryColor,
+        onTap: navigationTapped,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: _page == 0 ? brandColor : secondaryColor,
+              ),
+              label: 'Home',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.public,
+                color: _page == 1 ? brandColor : secondaryColor,
+              ),
+              label: 'Discover',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: _page == 2 ? brandColor : secondaryColor,
+              ),
+              label: 'Profile',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.notifications,
+                color: _page == 3 ? brandColor : secondaryColor,
+              ),
+              label: 'Notifications',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.menu,
+                color: _page == 4 ? brandColor : secondaryColor,
+              ),
+              label: 'Menu',
+              backgroundColor: primaryColor),
+        ],
+      ),
     );
   }
 }
